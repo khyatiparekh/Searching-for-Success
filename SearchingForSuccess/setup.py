0e8a9d20946cba0bf559f81290ac351182f4643e
Erin Orbits <orbite@uw.edu>
2017-06-05 17:45:10 -0700

    added import SearchingForSuccess

--- SearchingForSuccess/setup.py
+++ SearchingForSuccess/setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+import SearchingForSuccess
 
 here = path.abspath(path.dirname(__file__))
 
